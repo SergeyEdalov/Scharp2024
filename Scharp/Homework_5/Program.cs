f683e63faa5fd3d6707794ed2160f5d80ddadfd4
Sergey Edalov <edalov94@gmail.com>
2024-02-04 17:02:10 +0300

    Homework_5

---
+++ Scharp/Homework_5/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Доработайте программу калькулятор реализовав выбор действий и вывод результатов 
+            //на экран в цикле так чтобы калькулятор мог работать до тех пор пока пользователь 
+            //не нажмет отмена или введёт пустую строку.
+
+            Calculator calculator = new Calculator();
+            calculator.Result += Calculator_Result;
+            calculator.Add(10);
+            calculator.Add(35);
+            calculator.Div(3);
+            calculator.Mult(2);
+
+            Console.WriteLine();
+
+
+            calculator.Cancel();    
+            calculator.Cancel();    
+            calculator.Cancel();    
+            calculator.Cancel();    
+
+            calculator.Result -= Calculator_Result;
+            
+        }
+
+        private static void Calculator_Result(object? sender, CalculatorArgs e)
+        {
+            Console.Write($"{e.answer}  ");
+        }
+    }
+}
---
+++ Scharp/WpfApp1/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class CalculatorArgs : EventArgs
+    {
+        public int answer = 0;
+    }
+    class Calculator
+    {
+        public event EventHandler<CalculatorArgs> Result;
+        public int result { get; private set; }
+        Stack<int> results = new Stack<int>();
+
+        private void Calculation()
+        {
+            Result.Invoke(this, new CalculatorArgs { answer = result }); ;
+        }
+        public void Add(int value)
+        {
+            results.Push(result);
+            result += value;
+            Calculation();
+        }
+        public void Sub(int value)
+        {
+            results.Push(result);
+            result -= value;
+            Calculation();
+        }
+        public void Mult(int value)
+        {
+            results.Push(result);
+            result *= value;
+            Calculation();
+        }
+        public void Div(int value)
+        {
+            results.Push(result);
+            result /= value;
+            Calculation();
+        }
+
+        public void Cancel()
+        {
+            if (results.Count > 0)
+            {
+                result = results.Pop();
+                Calculation();
+            }
+        }
+    }
+}
