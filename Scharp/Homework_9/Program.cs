f5ddaba7ef4d41194c74dc3894a9db1c2e15da27
Sergey Edalov <edalov94@gmail.com>
2024-02-17 15:41:15 +0300

    Homework_9

---
+++ Scharp/Homework_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишите приложение, конвертирующее произвольный JSON в XML. Используйте JsonDocument.
+
+            JsonToXml("Example.json");
+        }
+
+        public static void JsonToXml(string fileName)
+        {
+            string json = File.ReadAllText(fileName);
+            //JsonDocument jsonDocument = JsonDocument.Parse(File.ReadAllText(fileName));
+
+            var xml = XDocument.Load(JsonReaderWriterFactory.CreateJsonReader
+                (Encoding.ASCII.GetBytes(json), new XmlDictionaryReaderQuotas()));
+
+            XDocument xmlDocument = new XDocument(xml);
+
+            Console.WriteLine(xmlDocument.ToString());
+
+            using (var writer = XmlWriter.Create("Example.xml"))
+                xmlDocument.WriteTo(writer);
+
+        }
+    }
+}
